7510ed7c313cfd6d6dfa8642d14870ad33ca3d69
Kumar Manas <krmanas0811@gmail.com>
2026-04-04 23:00:53 +0530

    Delete agents/orchestrator.py

--- agents/orchestrator.py
+++
@@ -1,9 +0,0 @@
-def route_query(query):
-    query = query.lower()
-
-    # Decide based on intent
-    if any(word in query for word in ["revenue", "sales", "product", "region"]):
-        return "data"
-
-    else:
-        return "rag"
