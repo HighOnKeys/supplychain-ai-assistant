26f1360004e595fca933a5ba39ab0178f07f4299
Kumar Manas <krmanas0811@gmail.com>
2026-04-14 00:16:22 +0530

    Update rag_pipeline.py

--- rag/rag_pipeline.py
+++ rag/rag_pipeline.py
@@ -30,7 +30,7 @@ def load_documents(folder_path):
 def split_documents(documents):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
-        chunk_overlap=50
+        chunk_overlap=100
     )
 
     chunks = splitter.split_documents(documents)
