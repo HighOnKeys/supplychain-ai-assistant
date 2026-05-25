967f3b241c18e69eebd4023fa4b38404e3e6f64d
Kumar Manas <krmanas0811@gmail.com>
2026-05-25 14:31:54 +0530

    Update smart_router.py

--- agents/smart_router.py
+++ agents/smart_router.py
@@ -27,7 +27,7 @@ Answer ONLY one word: DATA or RAG
 """
 
     response = client.models.generate_content(
-        model="gemini-3.5-flash",   # 🔥 IMPORTANT
+        model="gemini-3.5-flash-lite",   # 🔥 IMPORTANT
         contents=prompt
     )
 
